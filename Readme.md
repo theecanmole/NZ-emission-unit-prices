46fafcf44e12c4546303d8a020bbec1556e38b82
Theecanmole <theecanmole@gmail.com>
2023-10-28 17:59:14 +1300

    update

---
+++ Readme.md
@@ -0,0 +1,15 @@
+## New Zealand emission unit (NZU) prices
+
+This repository features New Zealand emission unit (NZU) prices web-scraped via Python (api.py file) and saved to a .csv file.
+
+The prices are then processed in the R programming language (nzu.r file) into data sets of average monthly price, average weekly price and spot prices.
+
+The price data is then charted in Base R and in Ggplot2.
+
+![](NZU-spot-prices-720by540.svg)
+
+![](NZUprice-720by540-ggplot.svg)
+
+![](NZU-monthly-spot-prices-720by540.svg)
+
+![](NZUprice-720by540-ggplot-theme-bw.svg)
---
+++ nzu.r
@@ -0,0 +1,196 @@
+nzu.r
+
+https://github.com/edi-rose/nzu-fork
+
+# in xterminal run "python3 api.py"
+
+# the csv file "nzu-edited-raw-prices-data.csv" is the output of 'api.py' (not "nzu-weekly-prices-data.csv" in the original folder)
+
+# "nzu-edited-raw-prices-data.csv" has the column headers ("date" "price" "reference" "month" "week") as the last row (line 1709) and not the first row...
+# load program aweek
+library(aweek)
+getwd()
+[1] "/home/user/R/nzu/nzu-fork-master/apipy"
+# read in data  reading the .csv file specifying header status as false
+data <- read.csv("nzu-edited-raw-prices-data.csv",header=FALSE)
+dim(data)
+[1] 1696    5
+# there is an extra row of data which is the first row
+str(data) 
+'data.frame':	1696 obs. of  5 variables:
+ $ V1: chr  "2010/05/14" "2010/05/21" "2010/05/29" "2010/06/11" ...
+ $ V2: chr  "17.75" "17.5" "17.5" "17" ...
+ $ V3: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
+ $ V4: chr  "2010/05/01" "2010/05/01" "2010/05/01" "2010/06/01" ...
+ $ V5: chr  "2010-W19" "2010-W20" "2010-W21" "2010-W23" ...
+
+# look at that last row again
+data[1696,]
+      V1    V2        V3    V4   V5
+1696 date price reference month week
+
+# look at last 6 rows
+tail(data,2) 
+             V1    V2                                                   V3
+1695 2023/10/27 69.30 https://www.carbonnews.co.nz/story.asp?storyID=29097
+1696       date price                                            reference
+          V4       V5
+1695 2023-10 2023-W43
+1696   month     week 
+
+# try converting the last row to a character vector
+as.character(data[1696,])
+[1] "date"      "price"     "reference" "month"     "week" 
+# add column names as character formated last row cells
+colnames(data) <- as.character(data[1696,])
+colnames(data)
+[1] "date"      "price"     "reference" "month"     "week" 
+head(data,2) 
+       date price                                          reference
+1 2010/05/14 17.75 http://www.carbonnews.co.nz/story.asp?storyID=4529
+2 2010/05/21  17.5 http://www.carbonnews.co.nz/story.asp?storyID=4540
+       month     week
+1 2010/05/01 2010/W19
+2 2010/05/01 2010/W20
+
+# look at last two rows
+# delete last row 1696 - the header names
+data <- data[-1696,]
+tail(data,1)
+tail(data,1)
+           date price                                            reference
+1695 2023/10/27 69.30 https://www.carbonnews.co.nz/story.asp?storyID=29097
+       month     week
+1695 2023-10 2023-W43
+
+# change formats of date column and price column
+data$date <- as.Date(data$date)
+data$price <- as.numeric(data$price)
+
+# add month variable/factor to data
+data$month <- as.factor(format(data$date, "%Y-%m"))
+
+# ok make aweek vector from date format column and overwrite contents of week column  
+data$week <- as.aweek(data$date) 
+str(data) 
+'data.frame':	1695 obs. of  5 variables:
+ $ date     : Date, format: "2010-05-14" "2010-05-21" ...
+ $ price    : num  17.8 17.5 17.5 17 17.8 ...
+ $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
+ $ month    : Factor w/ 162 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
+ $ week     : 'aweek' chr  "2010-W19-5" "2010-W20-5" "2010-W21-6" "2010-W23-5" ...
+  ..- attr(*, "week_start")= int 1
+ 
+# create new dataframe of monthly mean price 
+monthprice<-aggregate(price ~ month, data, mean)
+# round mean prices to whole cents
+monthprice[["price"]] = round(monthprice[["price"]], digits = 2)
+# replace month factor with mid-month 15th of month date-formatted object 
+monthprice[["month"]] = seq(as.Date('2010-05-15'), by = 'months', length = nrow(monthprice)) 
+
+# rename columns
+colnames(monthprice) <- c("date","price")
+str(monthprice) 
+'data.frame':	164 obs. of  2 variables:
+ $ date : Date, format: "2010-05-15" "2010-06-15" ...
+ $ price: num  17.6 17.4 18.1 18.4 20.1 ...
+
+# write the mean monthly price dataframe to a .csv file 
+
+write.table(monthprice, file = "nzu-month-price.csv", sep = ",", col.names = TRUE, qmethod = "double",row.names = FALSE) 
+
+# data <- data[,-6] # only needed once
+
+# remove week day part from aweek week and stay in aweek format
+data$week <- trunc(data$week) 
+  
+# create new dataframe of weekly mean price using 'aweek' variable 
+weeklyprice <- aggregate(price ~ week, data, mean)
+
+# round mean prices to whole cents
+weeklyprice[["price"]] = round(weeklyprice[["price"]], digits = 2)
+
+str(weeklyprice)
+
+# add date column from aweek week & change to date format 
+weeklyprice[["date"]] <- as.Date(weeklyprice[["week"]]) 
+  
+# change order of columns
+weeklyprice <- weeklyprice[,c(3,2,1)]
+
+str(weeklyprice)
+'data.frame':	608 obs. of  3 variables:
+ $ date : Date, format: "2010-05-10" "2010-05-17" ...
+ $ price: num  17.8 17.5 17.5 17 17.8 ...
+ $ week : 'aweek' chr  "2010-W19" "2010-W20" "2010-W21" "2010-W23" ...
+  ..- attr(*, "week_start")= int 1
+  
+# write the mean price per week dataframe to a .csv file 
+write.table(weeklyprice, file = "weeklymeanprice.csv", sep = ",", col.names = TRUE, qmethod = "double",row.names = FALSE)
+
+# check the dataframe again
+str(data) 
+'data.frame':	1695 obs. of  5 variables:
+ $ date     : Date, format: "2010-05-14" "2010-05-21" ...
+ $ price    : num  17.8 17.5 17.5 17 17.8 ...
+ $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
+ $ month    : Factor w/ 162 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
+ $ week     : 'aweek' chr  "2010-W19" "2010-W20" "2010-W21" "2010-W23" ...
+  ..- attr(*, "week_start")= int 1 
+
+# Create a .csv formatted data file
+write.csv(data, file = "nzu-final-prices-data.csv", row.names = FALSE) 
+
+str(monthprice) 
+'data.frame':	164 obs. of  2 variables:
+ $ date : Date, format: "2010-05-01" "2010-06-01" ...
+ $ price: num  17.6 17.4 18.1 18.4 20.1 ... 
+ 
+svg(filename="NZU-monthly-spot-prices-720by540.svg", width = 8, height = 6, pointsize = 14, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
+par(mar=c(2.7,2.7,1,1)+0.1)
+plot(monthprice[["date"]],monthprice[["price"]],tck=0.01,axes=TRUE,ann=TRUE, las=1,col="#F32424",lwd=2,type='l',lty=1) # color is Pomegranate not 'red'.
+grid(col="darkgray",lwd=1)
+axis(side=4, tck=0.01, las=0,tick=TRUE,labels = FALSE)
+mtext(side=1,cex=0.9,line=-1.3,"Data: 'NZU monthly prices' https://github.com/theecanmole/nzu")
+mtext(side=3,cex=1.2, line=-2.2,expression(paste("New Zealand Unit mean monthly spot prices 2010 - 2023")) )
+mtext(side=2,cex=1, line=-1.3,"$NZ Dollars/tonne")
+mtext(side=4,cex=0.75, line=0.05,R.version.string)
+dev.off()  
+
+
+svg(filename="NZU-spot-prices-720by540.svg", width = 8, height = 6, pointsize = 14, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
+par(mar=c(2.7,2.7,1,1)+0.1)
+plot(data[["date"]],data[["price"]],tck=0.01,axes=TRUE,ann=TRUE, las=1,col="#F32424",lwd=2,type='l',lty=1) # color is Pomegranate not 'red'.
+grid(col="darkgray",lwd=1)
+axis(side=4, tck=0.01, las=0,tick=TRUE,labels = FALSE)
+mtext(side=1,cex=0.9,line=-1.3,"Data: 'NZU monthly prices' https://github.com/theecanmole/nzu")
+mtext(side=3,cex=1.2, line=-2.2,expression(paste("New Zealand Unit spot prices 2010 - 2023")) )
+mtext(side=2,cex=1, line=-1.3,"$NZ Dollars/tonne")
+mtext(side=4,cex=0.75, line=0.05,R.version.string)
+dev.off() 
+
+#F32424 name is pomegranate which is 'creamy tomato' actually
+#ED1A3B is crimson
+library(ggplot2)
+
+svg(filename="NZUprice-720by540-ggplot.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
+#png("NZU-560by420-ggplot.png", bg="white", width=560, height=420,pointsize = 16)
+ggplot(data, aes(x = date, y = price)) +  geom_line(colour = "#F32424") +
+theme(plot.title = element_text(size = 17, hjust = 0.5)) +
+theme(plot.caption = element_text( hjust = 0.5 )) +
+labs(title="New Zealand Unit spot market prices 2010 - 2023", x ="Years", y ="Price $NZD", caption="Data: 'NZU prices' https://github.com/theecanmole/nzu") +
+#annotate("text", x=data[["date"]][1], y =1, size = 3, hjust =0, angle =0, label=R.version.string) +
+annotate("text", x=data[["date"]][1691], y =1, size = 3, hjust = 1,angle =0, label=R.version.string)
+dev.off()
+
+# "#ED1A3B" is Crimson 
+
+svg(filename="NZU-week-price-720by540-ggplot.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
+#png("NZU-week-560by420-ggplot.png", bg="white", width=560, height=420)
+ggplot(weeklyprice, aes(x = date, y = price)) +  geom_line(colour = "#F32424") +
+theme(plot.title = element_text(size = 17, hjust = 0.5)) +
+theme(plot.caption = element_text( hjust = 0.5 )) +
+labs(title="New Zealand Unit weekly mean spot market prices 2010 - 2023", x ="Years", y ="Price $NZD", caption="Data: 'NZU prices' https://github.com/theecanmole/nzu") +
+#annotate("text", x=data[["date"]][1], y =1, size = 3, hjust =0, angle =0, label=R.version.string) +
+annotate("text", x=weeklyprice[["date"]][608], y =2, size = 3, hjust = 1,angle =0, label=R.version.string)
+dev.off() 

66ce396d56a0b51694de59acf56abb7a2d5909b1
Theecanmole <theecanmole@gmail.com>
2023-10-29 19:15:59 +1300

    update

--- Readme.md
+++ Readme.md
@@ -13,3 +13,11 @@ The price data is then charted in Base R and in Ggplot2.
 ![](NZU-monthly-spot-prices-720by540.svg)
 
 ![](NZUprice-720by540-ggplot-theme-bw.svg)
+
+![](NZUprice-720by540-ggplot-theme-grey.svg)
+
+### License
+
+#### ODC-PDDL-1.0
+
+This data package and these datasets and the R scripts are made available under the Public Domain Dedication and License v1.0 whose full text can be found at: http://www.opendatacommons.org/licenses/pddl/1.0/. You are free to share, to copy, distribute and use the data, to create or produce works from the data and to adapt, modify, transform and build upon the data, without restriction.
--- nzu.r
+++ nzu.r
@@ -4,11 +4,16 @@ https://github.com/edi-rose/nzu-fork
 
 # in xterminal run "python3 api.py"
 
-# the csv file "nzu-edited-raw-prices-data.csv" is the output of 'api.py' (not "nzu-weekly-prices-data.csv" in the original folder)
+# the csv file "nzu-edited-raw-prices-data.csv" is the output of 'api.py'
 
-# "nzu-edited-raw-prices-data.csv" has the column headers ("date" "price" "reference" "month" "week") as the last row (line 1709) and not the first row...
-# load program aweek
-library(aweek)
+# "nzu-edited-raw-prices-data.csv" has the column headers ("date" "price" "reference" "month" "week") as the last row and not the first row..
+
+# load package aweek which deals with week  
+library("aweek")
+# load package Ggplot2 for graphs
+library("ggplot2")
+
+# check my current working folder
 getwd()
 [1] "/home/user/R/nzu/nzu-fork-master/apipy"
 # read in data  reading the .csv file specifying header status as false
@@ -90,16 +95,15 @@ monthprice[["month"]] = seq(as.Date('2010-05-15'), by = 'months', length = nrow(
 
 # rename columns
 colnames(monthprice) <- c("date","price")
+
+# check structure of dataframe
 str(monthprice) 
 'data.frame':	164 obs. of  2 variables:
  $ date : Date, format: "2010-05-15" "2010-06-15" ...
  $ price: num  17.6 17.4 18.1 18.4 20.1 ...
 
 # write the mean monthly price dataframe to a .csv file 
-
 write.table(monthprice, file = "nzu-month-price.csv", sep = ",", col.names = TRUE, qmethod = "double",row.names = FALSE) 
-
-# data <- data[,-6] # only needed once
 
 # remove week day part from aweek week and stay in aweek format
 data$week <- trunc(data$week) 
@@ -139,13 +143,20 @@ str(data)
   ..- attr(*, "week_start")= int 1 
 
 # Create a .csv formatted data file
-write.csv(data, file = "nzu-final-prices-data.csv", row.names = FALSE) 
+write.csv(data, file = "nzu-final-prices-data.csv", row.names = FALSE)
 
+# check the structure of the average monthly price data
 str(monthprice) 
 'data.frame':	164 obs. of  2 variables:
  $ date : Date, format: "2010-05-01" "2010-06-01" ...
  $ price: num  17.6 17.4 18.1 18.4 20.1 ... 
+
+# read back in to R the price data in three formats
+data <- read.csv("nzu-final-prices-data.csv", colClasses = c("Date","numeric","character","character","character"))
+monthprice <- read.csv("nzu-month-price.csv", colClasses = c("Date","numeric"))
+weeklyprice <- read.csv("weeklymeanprice.csv", colClasses = c("Date","numeric","character")) 
  
+# chart in Base R, 720 by 540,  
 svg(filename="NZU-monthly-spot-prices-720by540.svg", width = 8, height = 6, pointsize = 14, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
 par(mar=c(2.7,2.7,1,1)+0.1)
 plot(monthprice[["date"]],monthprice[["price"]],tck=0.01,axes=TRUE,ann=TRUE, las=1,col="#F32424",lwd=2,type='l',lty=1) # color is Pomegranate not 'red'.
@@ -157,7 +168,29 @@ mtext(side=2,cex=1, line=-1.3,"$NZ Dollars/tonne")
 mtext(side=4,cex=0.75, line=0.05,R.version.string)
 dev.off()  
 
+# This is the closest to my preferred base R chart - it is in the Ggplot2 theme 'black and white' 
+svg(filename="NZUprice-720by540-ggplot-theme-bw.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
+#png("NZU-560by420-ggplot-theme-bw.png", bg="white", width=560, height=420)
+ggplot(monthprice, aes(x = date, y = price)) +  geom_line(colour = "#ED1A3B") +
+theme_bw(base_size = 14) +
+theme(plot.title = element_text(size = 20, hjust = 0.5,vjust= -10 )) +
+theme(plot.caption = element_text( hjust = 0.5 )) +
+labs(title="New Zealand Mean Monthly Unit Prices 2010 - 2023", x ="Years", y ="Price $NZD", caption="Data: 'NZU monthly prices' https://github.com/theecanmole/nzu") +
+annotate("text", x= monthprice[["date"]][161], y = 2, size = 3, angle = 0, hjust = 1, label=R.version.string)
+dev.off()
 
+# This is in the Ggplot2 default theme 
+svg(filename="NZUprice-720by540-ggplot-theme-grey.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
+#png("NZU-560by420-ggplot-theme-bw.png", bg="white", width=560, height=420)
+ggplot(monthprice, aes(x = date, y = price)) +  geom_line(colour = "#ED1A3B") +
+theme_gray(base_size = 14) +
+theme(plot.title = element_text(size = 20, hjust = 0.5,vjust= -10 )) +
+theme(plot.caption = element_text( hjust = 0.5 )) +
+labs(title="New Zealand Mean Monthly Unit Prices 2010 - 2023", x ="Years", y ="Price $NZD", caption="Data: 'NZU monthly prices' https://github.com/theecanmole/nzu") +
+annotate("text", x= monthprice[["date"]][161], y = 2, size = 3, angle = 0, hjust = 1, label=R.version.string)
+dev.off()
+
+# graph of spot prices in Base R
 svg(filename="NZU-spot-prices-720by540.svg", width = 8, height = 6, pointsize = 14, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
 par(mar=c(2.7,2.7,1,1)+0.1)
 plot(data[["date"]],data[["price"]],tck=0.01,axes=TRUE,ann=TRUE, las=1,col="#F32424",lwd=2,type='l',lty=1) # color is Pomegranate not 'red'.
@@ -171,7 +204,7 @@ dev.off()
 
 #F32424 name is pomegranate which is 'creamy tomato' actually
 #ED1A3B is crimson
-library(ggplot2)
+
 
 svg(filename="NZUprice-720by540-ggplot.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
 #png("NZU-560by420-ggplot.png", bg="white", width=560, height=420,pointsize = 16)
